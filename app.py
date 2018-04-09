eaffa84c0f6d9e24c73204be32d89fd67c4123bd
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-04-09 05:50:09 +0000

    Either merge this or be prepared to launch the warp core.

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'ba7678cd11d08bfd135092fe953adc08'
-__version__ = 'f82857a39c985f5bacdbf20804d48b8c'
+__vcs_id__ = '85fe958c9b5a2ddd941d561494d1c497'
+__version__ = 'dfa68403c9861812b746006413666443'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
