2abb730c9ad9f12bda7d62ddfa340c6df50f9cf3
Unicorn Rentals <31566919+unicorn-rental-cicd@users.noreply.github.com>
2018-04-09 15:30:38 +0000

    This commit is a lie.

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ Probably better way to do this, but for now this will do.
 """
 
 __author__ = 'Inigo Montoya (inigo.montoya@unicornrentals.click)'
-__vcs_id__ = 'd57618dd90871f347ee8b914c03d439b'
-__version__ = '9d21ede51834f3cce780f99fb8a87a87'
+__vcs_id__ = '522e12b0d013e483703bb598e7ee78fc'
+__version__ = 'c0b67ddd1f0240531d39d96814be7c4d'
 
 from flask import Flask, request
 from flask_restful import Resource, Api
